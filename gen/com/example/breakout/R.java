694d8e8b4b7699fa0d69c5dffc08a45bad4d6fec
Neil Armstrong <neil_armstrong_1991@hotmail.com>
2014-03-24 14:54:40 +0000

    Test commit

--- gen/com/example/breakout/R.java
+++ gen/com/example/breakout/R.java
@@ -24,8 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f080001;
-        public static final int Button02=0x7f080002;
+        public static final int Button01=0x7f080002;
+        public static final int Button02=0x7f080001;
         public static final int action_settings=0x7f080003;
         public static final int button1=0x7f080000;
     }
